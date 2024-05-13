bd6d3647ff55de8918abcef8b527c2d9a93183cd
Illya <bernstakiy.i@gmail.com>
2024-05-13 18:01:24 +0300

    finish test_8

---
+++ test8.js
@@ -0,0 +1,53 @@
+let blockProperty = {
+    color: 'black',
+    opacity: 0,
+};
+
+function editProperty(object,key,valye){
+    // создаю новый объект с помощью оператора spread копирую свойства из исходного объект
+    let newObj= {...object};
+    // устанавливаю значение нового свойства.
+    newObj[key]=valye;
+    return newObj;
+}
+console.log('blockProperty:',blockProperty);
+console.log('editProperty:',editProperty(blockProperty, 'color', 'red'));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let blockProperty = {
+//     color: 'black',
+//     opacity: 0,
+// };
+
+// function editProperty(object, key, value) {
+//     // Создаем новый объект с помощью оператора spread копирую свойства из исходного объект
+//     let newObj = { ...object };
+//     // Затем устанавливаю значение нового свойства.
+//     newObj[key] = value;
+
+//     return newObj;
+// }
+
+// console.log('blockProperty:',blockProperty);
+// console.log('editProperty:',editProperty(blockProperty, 'color', 'red'));
+
